770e28878d91ba0d27a22158c91b7813d4f3e9d9
Amir Fahd Hadji Usop <dudeamirfahd@gmail.com>
2020-01-25 04:47:55 +0800

    Set up project and its architecture

---
+++ app/src/main/java/com/codingchallenge/itunesviewer/views/SplashActivity.kt
@@ -0,0 +1,15 @@
+package com.codingchallenge.itunesviewer.views
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.codingchallenge.itunesviewer.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        
+    }
+}
